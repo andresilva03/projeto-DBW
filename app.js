ec19b63e6e10d6a5c64d1d75785fdd60257d5cec
Pedro Costa <jpscosta002@gmail.com>
2024-04-12 02:36:45 +0100

    first commit

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require('express');
 
 const app = express();
 
+var path = require('path');
 
 app.listen(3000);
 
 app.set('view engine','ejs');
 
-app.get('/',(req,res) =>{
+app.use(express.static(path.join(__dirname,'public')));
+
+
+app.get('/login',(req,res) =>{
     res.render('login')
+});
+
+app.get('/',(req,res) =>{
+    res.render('index')
 });
