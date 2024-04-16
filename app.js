b98f59b767d69ebd5539860ac35aafb71b4d379d
andresilva03 <151201314+andresilva03@users.noreply.github.com>
2024-04-16 19:38:18 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -10,11 +10,22 @@ app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname,'public')));
 
-
 app.get('/login',(req,res) =>{
     res.render('login')
 });
 
 app.get('/',(req,res) =>{
     res.render('index')
+});
+
+app.get('/TermsandConditions',(req,res) =>{
+    res.render('TermsandConditions')
+});
+
+app.get('/aboutus',(req,res) =>{
+    res.render('aboutus')
+});
+
+app.get('/tournaments',(req,res) =>{
+    res.render('tournaments')
 });
